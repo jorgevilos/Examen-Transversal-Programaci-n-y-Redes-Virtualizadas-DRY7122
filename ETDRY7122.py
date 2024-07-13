19abd14f602e491e114d50e0f681e7b22a907478
jorgevilos <jo.vilos@duocuc.cl>
2024-07-13 22:29:07 +0000

    Committing ETDRY7122.py to begin tracking changes

---
+++ ETDRY7122.py
@@ -0,0 +1,9 @@
+# integrantes.py
+integrantes = [
+    {"nombre": "Cristian", "apellido": "Ambos"},
+    {"nombre": "Washington", "apellido": "Gonzalez"},
+    {"nombre": "Jorge", "apellido": "Vilos"},
+]
+
+for integrante in integrantes:
+    print(f"Nombre: {integrante['nombre']}, Apellido: {integrante['apellido']}")
